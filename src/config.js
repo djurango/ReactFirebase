2936b7683ed6c3d7ee0566efd995edd2ee5ec9c2
djurango <djuric@me.com>
2019-11-06 19:08:47 +0100

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,3 +1,9 @@
 export default {
-  apiKey: "XXX"
+  "projectId": "todo-37b15",
+  "databaseURL": "https://todo-37b15.firebaseio.com",
+  "storageBucket": "todo-37b15.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyABVec1R1RTy0-FRsfKpy2yrH559WHfri8",
+  "authDomain": "todo-37b15.firebaseapp.com",
+  "messagingSenderId": "840328702056"
 };
